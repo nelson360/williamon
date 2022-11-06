4de8bae304a56e9e210d893f23bda7a26b4944ac
nelson360 <nelsonomondi99@gmail.com>
2022-11-06 21:46:56 +0300

    d

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,80 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/AboutComponent">About</router-link><br>
-    <router-link to="/QualificationComponent">qualification</router-link><br>
-    <router-link to="/ContactComponent">Contact</router-link><br>
+<nav>
+    <ul>
+        <li>
+            <router-link to="/App">Home</router-link>
+        </li>
+        <li>
+            <router-link to="/AboutComponent">About</router-link>
+        </li>
+        <li>
+            <router-link to="/QualificationComponent">qualification</router-link>
+        </li>
+        <li>
+            <router-link to="/ContactComponent">Contact</router-link>
+        </li>
+        <RouterView />
 </nav>
-<RouterView />
-</template>
-<script>
 
+</template>
+
+<script>
 export default {
-  name: 'HeaderComponent',
-  
+    name: 'HeaderComponent',
+
 }
 </script>
 
+<style>
+nav {
+    width: 100%;
+    line-height: 1.4em;
+}
 
-<style></style>
+ul {
+    list-style: none;
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    text-align: justify;
+    margin-bottom: -1.4em;
+}
+
+ul:after {
+    content: "";
+
+    color: rgb(20, 182, 33);
+    display: inline-block;
+    width: 100%;
+}
+
+li {
+    display: inline-block;
+    margin: 4%;
+    padding: auto;
+    font: size 25px;
+    font-style: normal;
+    color: rgb(20, 182, 33);
+    text-decoration: none;
+
+}
+
+nav>a {
+    font-weight: bold;
+    color: #2c3e50;
+    /* &.router-link-exact-active {
+    color: #42b983;
+  } */
+}
+
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+body {
+    margin-bottom: 60px;
+    /* Margin bottom by footer height */
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,7 @@
+import {createstore} from 'vuex'
+export default createstore({
+    state:{},
+    mutations:{},
+    actions:{},
+    modules:{}
+})
